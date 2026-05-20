823e950e1de7aa059f75465d08b6089c02be197e
Alexsandro <alexsandro.carvalho821@gmail.com>
2026-05-20 20:02:07 -0300

    feat: adiciona endpoint para listar assets

--- src/main/java/com/project/smart_wallet/controller/AssetController.java
+++ src/main/java/com/project/smart_wallet/controller/AssetController.java
@@ -1,16 +1,20 @@
 package com.project.smart_wallet.controller;
 
 import com.project.smart_wallet.dto.request.CreateAssetRequest;
+import com.project.smart_wallet.dto.response.AssetSummaryResponse;
 import com.project.smart_wallet.dto.response.CreateAssetResponse;
 import com.project.smart_wallet.service.AssetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/assets")
@@ -23,6 +27,12 @@ public class AssetController {
     @ResponseStatus(HttpStatus.CREATED)
     public CreateAssetResponse createAsset(@Valid @RequestBody CreateAssetRequest request) {
         return assetService.create(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<AssetSummaryResponse> listAssets() {
+        return assetService.listAll();
     }
 
 }
--- src/main/java/com/project/smart_wallet/service/AssetService.java
+++ src/main/java/com/project/smart_wallet/service/AssetService.java
@@ -2,11 +2,15 @@ package com.project.smart_wallet.service;
 
 import com.project.smart_wallet.domain.Asset;
 import com.project.smart_wallet.dto.request.CreateAssetRequest;
+import com.project.smart_wallet.dto.response.AssetSummaryResponse;
 import com.project.smart_wallet.dto.response.CreateAssetResponse;
 import com.project.smart_wallet.exception.ConflictException;
+import com.project.smart_wallet.mapper.AssetSummaryMapper;
 import com.project.smart_wallet.repository.AssetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.project.smart_wallet.mapper.CreateAssetMapper.toEntity;
 import static com.project.smart_wallet.mapper.CreateAssetMapper.toResponse;
@@ -28,6 +32,12 @@ public class AssetService {
         assetRepository.save(asset);
 
         return toResponse(asset);
+    }
+
+    public List<AssetSummaryResponse> listAll() {
+        return assetRepository.findAll().stream()
+                .map(AssetSummaryMapper::toResponse)
+                .toList();
     }
 
 }
