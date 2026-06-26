a71512b7ab157ec7bcf30de26f9038ed11e77e03
Alexsandro <alexsandro.carvalho821@gmail.com>
2026-06-25 22:54:58 -0300

    refactor: altera objeto de retorno e entrada de api externa

--- src/main/java/com/project/smart_wallet/client/CoingeckoClient.java
+++ src/main/java/com/project/smart_wallet/client/CoingeckoClient.java
@@ -1,17 +1,19 @@
 package com.project.smart_wallet.client;
 
-import com.project.smart_wallet.client.dto.AssetPriceResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import static com.project.smart_wallet.client.mapper.CoingeckoMapper.toAssetResponse;
+
 @Component
-public class CoingeckoClient {
+public class CoingeckoClient implements AssetPriceProvider {
 
     private static final String CURRENCY = "brl";
 
@@ -21,17 +23,21 @@ public class CoingeckoClient {
         this.webClient = webClient;
     }
 
-    public CompletableFuture<Map<String, AssetPriceResponse>> getPricePerAsset(String assets) {
+    @Override
+    public CompletableFuture<Map<String, BigDecimal>> getPricePerAsset(List<String> assets) {
+        String ids = String.join(",", assets);
+
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/simple/price")
-                        .queryParam("ids", assets)
+                        .queryParam("ids", ids)
                         .queryParam("vs_currencies", CURRENCY)
                         .build())
                 .retrieve()
                 .bodyToMono(
-                        new ParameterizedTypeReference<Map<String, AssetPriceResponse>>() {
+                        new ParameterizedTypeReference<Map<String, Map<String, BigDecimal>>>() {
                         })
+                .map(response -> toAssetResponse(response, CURRENCY))
                 .toFuture();
     }
 }
