b6e7b99a90dd6495cfd3e8e3263f2c45d2c55b62
Alexsandro <alexsandro.carvalho821@gmail.com>
2026-06-25 23:03:14 -0300

    feat: adiciona integração com api externa de stock

--- src/main/java/com/project/smart_wallet/service/WalletService.java
+++ src/main/java/com/project/smart_wallet/service/WalletService.java
@@ -1,7 +1,7 @@
 package com.project.smart_wallet.service;
 
+import com.project.smart_wallet.client.BrapiClient;
 import com.project.smart_wallet.client.CoingeckoClient;
-import com.project.smart_wallet.client.dto.AssetPriceResponse;
 import com.project.smart_wallet.domain.AssetType;
 import com.project.smart_wallet.domain.User;
 import com.project.smart_wallet.dto.projection.AssetBalanceProjection;
@@ -11,11 +11,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
+import static com.project.smart_wallet.domain.AssetType.CRYPTO_CURRENCY;
+import static com.project.smart_wallet.domain.AssetType.STOCK;
 
 @Service
 @RequiredArgsConstructor
@@ -27,30 +31,50 @@ public class WalletService {
 
     private final CoingeckoClient coingeckoClient;
 
+    private final BrapiClient brapiClient;
+
     public BalanceResponse getBalance() {
         User user = userService.getAuthenticatedUser();
 
         List<AssetBalanceProjection> assetsBalance = transactionRepository.getBalance(user.getId());
 
-        String cryptoAssets = assetsBalance.stream()
-                .filter(asset -> asset.assetType() == AssetType.CRYPTO_CURRENCY)
-                .map(AssetBalanceProjection::assetName)
-                .collect(Collectors.joining(","));
+        List<String> cryptoAssetsName = filterByAssetType(
+                assetsBalance,
+                CRYPTO_CURRENCY,
+                AssetBalanceProjection::assetName
+        );
 
-        Map<String, AssetPriceResponse> cryptoAssetsPrice = coingeckoClient.getPricePerAsset(cryptoAssets);
+        List<String> stocksAssetsSymbol = filterByAssetType(
+                assetsBalance,
+                STOCK,
+                AssetBalanceProjection::assetSymbol
+        );
+
+        CompletableFuture<Map<String, BigDecimal>> cryptoAssetsPriceFuture = !cryptoAssetsName.isEmpty()
+                ? coingeckoClient.getPricePerAsset(cryptoAssetsName)
+                : CompletableFuture.completedFuture(Collections.emptyMap());
+
+        CompletableFuture<Map<String, BigDecimal>> stocksAssetsPriceFuture = !stocksAssetsSymbol.isEmpty()
+                ? brapiClient.getPricePerAsset(stocksAssetsSymbol)
+                : CompletableFuture.completedFuture(Collections.emptyMap());
+
+        CompletableFuture.allOf(cryptoAssetsPriceFuture, stocksAssetsPriceFuture).join();
+
+        Map<String, BigDecimal> cryptoAssetsPrice = cryptoAssetsPriceFuture.join();
+        Map<String, BigDecimal> stocksAssetsPrice = stocksAssetsPriceFuture.join();
 
         BigDecimal totalBalance = BigDecimal.ZERO;
 
         for (AssetBalanceProjection assetBalance : assetsBalance) {
-            AssetPriceResponse currentAsset = null;
-            BigDecimal currentAssetPrice = assetBalance.averagePrice();
+            BigDecimal currentAssetPrice = null;
 
-            if (assetBalance.assetType() == AssetType.CRYPTO_CURRENCY) {
-                currentAsset = cryptoAssetsPrice.get(assetBalance.assetName());
+             switch (assetBalance.assetType()) {
+                case CRYPTO_CURRENCY -> currentAssetPrice = cryptoAssetsPrice.get(assetBalance.assetName());
+                case STOCK -> currentAssetPrice = stocksAssetsPrice.get(assetBalance.assetSymbol());
             }
 
-            if (currentAsset != null) {
-                currentAssetPrice = currentAsset.price();
+            if (currentAssetPrice == null) {
+                currentAssetPrice = assetBalance.averagePrice();
             }
 
             totalBalance = totalBalance.add(currentAssetPrice.multiply(assetBalance.quantity()));
@@ -67,4 +91,16 @@ public class WalletService {
                 totalBalance.subtract(totalSpending)
         );
     }
+
+    private List<String> filterByAssetType(
+            List<AssetBalanceProjection> assets,
+            AssetType type,
+            Function<AssetBalanceProjection,String> mapMethod
+    ) {
+        return assets.stream()
+                .filter(asset -> asset.assetType() == type)
+                .map(mapMethod)
+                .toList();
+    }
+
 }
