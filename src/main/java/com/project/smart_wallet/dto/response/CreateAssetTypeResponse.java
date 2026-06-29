30c6be89634373805af8a9579338ca605cc715b3
Alexsandro <alexsandro.carvalho821@gmail.com>
2026-06-28 22:48:39 -0300

    refactor: remove DTO não utilizado

--- src/main/java/com/project/smart_wallet/dto/response/CreateAssetTypeResponse.java
+++
@@ -1,6 +0,0 @@
-package com.project.smart_wallet.dto.response;
-
-import java.time.Instant;
-
-public record CreateAssetTypeResponse(Long id, String name, Boolean incomeGenerating, Instant createdAt) {
-}
